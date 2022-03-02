8074f921c3d264cfdc7705a75d0a98163f5d5af2
Saurabh Sharma <saurabh182000@gmail.com>
2022-03-02 19:59:05 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// listen for submit button to create question
+// save question to the storage
+// append question to the left panel
+// listen for click on question and display in right pane
+// listen for click on submit respone Button
+// display response in response section
+
+function onQuestionSubmit() {
+    
+}
